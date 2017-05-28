1e38ade065944fd3829ec1c41df40b997db969b3
Aakash Bhatia <akash.bhatia1184@gmail.com>
2017-05-28 13:16:44 +1000

    Answer 1

--- testquiz/contollers/QuizController.php
+++ testquiz/contollers/QuizController.php
@@ -12,8 +12,13 @@ class QuizController extends Quiz
 		return 'QuizController ready';
 	}
 
-	public function query1($entry) {
-		$this->answers[] = $entry;
+	public function query1(string $entry) {
+		if ( ctype_upper($entry[0]) ) {
+			$this->answers[] = 'The provided string starts with a uppercase letter';
+		} else {
+			$this->answers[] = 'The provided string does not start with a uppercase letter';
+
+		}
 		
 		return $this->answers[0];
 	}
